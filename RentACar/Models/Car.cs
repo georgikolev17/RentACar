9b50b4e33e2e158659ae73eb1a8498974616b5f0
martin <martin@chtilianov.com>
2024-04-17 17:53:14 +0300

    Added Model for Car

---
+++ RentACar/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace RentACar.Models
+{
+    public class Car
+    {
+        public Car(string? make, string? model, string? year, int numPassengers, string? description, int pricePerDay)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            NumPassengers = numPassengers;
+            Description = description;
+            PricePerDay = pricePerDay;
+            CarUUID = Guid.NewGuid().ToString();
+        }
+
+        public string CarUUID;
+
+        public string? Make { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Year { get; set; }
+
+        public int NumPassengers { get; set; }
+
+        public string? Description { get; set; }
+
+        public int PricePerDay { get; set; }
+    }
+}
