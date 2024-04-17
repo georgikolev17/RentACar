9820b32871c526cb7ba57729f11f99b8bb3b9488
martin <martin@chtilianov.com>
2024-04-17 17:53:40 +0300

    Added Model for CarRequests

---
+++ RentACar/Models/CarRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace RentACar.Models
+{
+    public class CarRequest
+    {
+        public CarRequest(string? userID, string? carID, DateOnly startDate, DateOnly endDate)
+        {
+            UserID = userID;
+            CarID = carID;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public string? UserID;
+
+        public string? CarID;
+
+        public DateOnly StartDate;
+        
+        public DateOnly EndDate;
+    }
+}
